1c7030c8caa46cbda7a81a871c63e24279edf147
jormd <johann.romand@arengi.fr>
2023-02-10 09:28:35 +0100

    compatibilité sf 44 et php8

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -7,7 +7,7 @@ define("DB_HOST", getenv("DB_HOST") ?: "localhost");
 define("DB_PORT", getenv("DB_PORT") ?: null);
 define('DB_NAME', getenv("DB_NAME") ?: "lexik_translation_test");
 define("DB_USER", getenv("DB_USER") ?: "root");
-define("DB_PASSWD", getenv("DB_PASSWD") ?: null);
+define("DB_PASSWD", getenv("DB_PASSWD") ?: "Adweita2016$");
 
 if (file_exists($file = __DIR__.'/autoload.php')) {
     require_once $file;
